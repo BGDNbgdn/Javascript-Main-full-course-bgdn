d63bffcd4cc01ff811387b2e949a28c6611c20e6
BGDNbgdn <bogdan.dmytrenko15@gmail.com>
2024-08-01 19:26:47 +0200

    updt

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,2 +1,12 @@
 // Remember, we're gonna use strict mode in all scripts now!
 "use strict";
+
+function printForecast(arr) {
+	let forecastSentence = "";
+	for (let i = 0; i < arr.length; i++) {
+		forecastSentence += ` ... ${arr[i]}C in ${i + 1} days`;
+	}
+	return forecastSentence;
+}
+
+console.log(printForecast([17, 21, -5, 23]));
