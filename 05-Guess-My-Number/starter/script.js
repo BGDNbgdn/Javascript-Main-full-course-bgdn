82e5aad0a1253e6632f3fa1564e79cb8bab3049e
BGDNbgdn <bogdan.dmytrenko15@gmail.com>
2024-08-03 19:46:05 +0200

    updt script part 4

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -26,7 +26,7 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.highscore').textContent = highscore;
     }
 
-    //when guess is rong
+    //when guess is wrong
   } else if (guess !== secretNumber) {
     if (score > 1) {
       displayMessage(guess > secretNumber ? 'Too high!' : 'Too low!');
