3ff3187fd594bbabed0592957b4cdca9c9d97fc8
BGDNbgdn <bogdan.dmytrenko15@gmail.com>
2024-08-13 22:08:02 +0200

    UPD

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -47,34 +47,3 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
-
-//Short circuiting && and ||
-//Use any data type, return ANY data type, short circuiting
-
-console.log('' || undefined || 0 || 'Jonas' || 23); //returns Jonas since the first values are falsy
-
-restaurant.numGuests = 23;
-const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
-console.log(guests1); // guests = 23
-
-const guests2 = restaurant.numGuests || 10;
-console.log(guests2); //returns 23 since its a true value, othervise if numGuests didnt exist we would get 10, doesnt work if number of guests is 0, we can use it to set default values, see the next part to solve this
-
-//AND Operator
-console.log(0 && 'Jonas'); // returns 0, it returns the first falsy value found, if all operants are true returns the last truthy one, so Jonas
-
-if (restaurant.orderPizza) {
-  restaurant.orderPizza('mushrooms', 'spinach');
-} // returns since restaurant.orderPizza exists
-
-restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach'); //another way to call a ufnction, if orderPizza exists, the && operator will return the last truthy value, so it will execute code in the second operant if the first one is true.
-
-//THE NULLISH COALESCING OPERATOR
-restaurant.numGuests = 0;
-const guests3 = restaurant.numGuests || 10;
-console.log(guests3);
-
-//Nullish : null and undefined (NOT 0 or '')
-// ?? is same as || but in this case accepts 0 or '' as truthy values
-const guests4 = restaurant.numGuests ?? 10;
-console.log(guests4);
