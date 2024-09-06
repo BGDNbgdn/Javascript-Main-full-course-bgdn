765fb07faeaf6afe0321d08c73a18025a05de2e0
Bogdan DMYTRENKO <108947667+BGDNbgdn@users.noreply.github.com>
2024-09-05 22:50:35 -0400

    upd

--- 09-Data-Structures-Operators/final/script.js
+++ 09-Data-Structures-Operators/final/script.js
@@ -107,8 +107,8 @@ document.body.append(document.createElement('textarea'));
 document.body.append(document.createElement('button'));
 
 document.querySelector('button').addEventListener('click', function () {
-  const text = document.querySelector('textarea').value;
-  const rows = text.split('\n'    
+  const text = document.querySelector('textarea ').value;
+  const rows = text.split('\n'     
 
   for (const [i, row] of rows.entries()) {
     const [first, second] = row.toLowerCase().trim().split('_');
